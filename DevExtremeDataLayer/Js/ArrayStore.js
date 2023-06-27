a3863dcc56a8ef124fb66092e63a9647adbc270f
Om Joshi <om@rkitsoftware.com>
2023-06-27 18:24:11 +0530

    implement data grid in drop down but still need to develop simple demo to understand easily

---
+++ DevExtremeDataLayer/Js/ArrayStore.js
@@ -0,0 +1,5 @@
+﻿$(() => {
+
+    //Data Layer
+    const dataSource
+})
--- DevExtremeEditors/Js/DropDown.js
+++ DevExtremeEditors/Js/DropDown.js
@@ -1,19 +1,45 @@
 ﻿$(() => {
-    const car = ["Baleno", "Ciaz", "Jimnny", "Thar", "Elantra"];
+    const companyArray = (jsonFile) => {
+        return new DevExpress.data.CustomStore({
+            loadMode: 'raw',
+            key: "ID",
+            load() {
+                return $.getJSON(`../Data/${jsonFile}`);
+            }
 
-    $("#myDrop").dxDropDown({
+        })
+    }
+
+    $("#myDrop").dxDropDownBox({
+        dataSource: companyArray('companyList.json'),
+        displayExpr(item) {
+            return item && `${item.CompanyName} <${item.Phone}>`;
+        },
         contentTemplate: (e) => {
-            const $list = $("<div>").dxList({
-                car,
-                allowItemDeleting: true,
-                onItemDeleting: (e) => {
-                    if (dataSource.length === 1) {
-                        e.cancel = true;
-                    }
-                }
-            })
-            list = $list.dxList('instance');
-            return $list;
+            const value = e.component.option('value');
+            const $dataGrid = $('<div>').dxDataGrid({
+                dataSource: e.component.getDataSource(),
+                columns: ['CompanyName', 'City', 'Phone'],
+                filterRow: { visible: true },
+                selection: { mode: 'single' },
+                selectedRowKeys: [value],
+                selectNodesRecursive: false,
+                onSelectionChanged(selectedItems) {
+                    const keys = selectedItems.selectedRowKeys;
+                    const hasSelection = keys.length;
+
+                    e.component.option('value', hasSelection ? keys[0] : null);
+                },
+            });
+            dataGrid = $dataGrid.dxDataGrid('instance');
+
+
+            e.component.on('valueChanged', (args) => {
+                dataGrid.selectRows(args.value, false);
+                e.component.close();
+            });
+
+            return $dataGrid;
         }
-    })
+    })  
 })
