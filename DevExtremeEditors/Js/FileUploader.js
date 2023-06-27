4adb80f4e798d4d7374d70c1e4d6052352373f98
Om Joshi <om@rkitsoftware.com>
2023-06-27 16:28:09 +0530

    File Uploader

---
+++ DevExtremeEditors/Js/FileUploader.js
@@ -0,0 +1,34 @@
+﻿$(() => {
+    $("#dxFileUpload").dxFileUploader({
+        multiple: false,
+        selectButtonText: "Select File",
+        allowCanceling: true,
+        //accept: "image/png",
+        chunkSize: 200000,
+        maxFileSize: 4000000,
+        allowCanceling: true,
+        allowedFileExtensions: [".png"],
+        invalidFileExtensionMessage:"you have to select png file only",
+        uploadUrl: 'https://js.devexpress.com/Demos/NetCore/FileUploader/Upload',
+        uploadMode: "instantly",
+        uploadAbortedMessage:"upload cancelled with all of the heart of user",
+        inputAttr: {
+            id: "FileIploader"
+        },
+
+        onBeforeSend: (e) => {
+            console.log(e);
+            console.log(e.file.type)
+        },
+
+        onFilesUploaded: () => {
+            $("#msg").text("File Uploaded");
+        },
+
+        onUploadError: (e) => {
+            const fileType = e.file.type
+            $("#error").text("only" + fileType + "is accepted");
+        }
+    }).dxFileUploader("instance");
+
+})
