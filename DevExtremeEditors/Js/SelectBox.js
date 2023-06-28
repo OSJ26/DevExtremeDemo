c0aadaf2bb3e12064f9ae9a86abec0293e30ecc7
Om Joshi <om@rkitsoftware.com>
2023-06-28 18:56:25 +0530

    Grouped in select box

--- DevExtremeEditors/Js/SelectBox.js
+++ DevExtremeEditors/Js/SelectBox.js
@@ -45,4 +45,51 @@
             console.log("value Changed", e.value);
         }
     })
+
+    const unGrouped = [{
+        ID: 1,
+        Name: 'Om',
+        Category: 'Admin',
+    },
+    {
+        ID: 2,
+        Name: 'Kishan',
+        Category: 'Admin',
+    },
+    {
+        ID: 3,
+        Name: 'Hemangi',
+        Category: 'User',
+    },
+    {
+        ID: 4,
+        Name: 'Shivam',
+        Category: 'User',
+    },
+    {
+        ID: 5,
+        Name: 'Akhil',
+        Category: 'User',
+    }
+    ]
+
+    const fromUngroupedData = new DevExpress.data.DataSource({
+        store: {
+            type: 'array',
+            data: unGrouped,
+            key: 'ID',
+        },
+        group: 'Category',
+    });
+
+    $("#groupedSelect").dxSelectBox({
+        dataSource: fromUngroupedData,
+        placeholder: "Choose User",
+        showClearButton: true,
+        width: "200px",
+        searchEnabled: true,
+        displayExpr: "Name",
+        valueExpr: "id",
+        grouped: true
+    })
 })
