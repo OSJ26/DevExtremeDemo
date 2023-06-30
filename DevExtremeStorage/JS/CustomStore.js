5e9b193b19147d1c7fc05a9a8a7ddcc4bc86d755
Om Joshi <om@rkitsoftware.com>
2023-06-30 15:39:14 +0530

    Item Creation when ajax call is successed

--- DevExtremeStorage/JS/CustomStore.js
+++ DevExtremeStorage/JS/CustomStore.js
@@ -9,7 +9,16 @@
                 type: "GET",
                 url: "https://localhost:7238/api/USED01",
                 success: (e) => {
-                    console.log(e);
+                    $('#myList').dxSelectBox({
+                        dataSource: myStore,
+                        acceptCustomValue: true,
+                        displayExpr: 'd01F02',
+                        valueExpr: 'd01F01',
+                        height: '100%',
+                        width: '300px',
+                        hint: 'Employee List',
+                        showClearButton: true,
+                    });
                     DevExpress.ui.notify("Data Fetched SUccessfully", "success", 500);
                 },
                 error: (e) => {
@@ -19,17 +28,6 @@
             })
         },
         loadMode: "raw",
-    });
-
-    await $('#myList').dxSelectBox({
-        dataSource: myStore,
-        acceptCustomValue: true,
-        displayExpr: 'd01F02',
-        valueExpr: 'd01F01',
-        height: '100%',
-        width: '300px',
-        hint: 'Employee List',
-        showClearButton: true,
     });
 
 })
