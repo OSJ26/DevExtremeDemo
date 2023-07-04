698bbf762193d7d57c315fb7d96b1a649d6b1c6a
Om Joshi <om@rkitsoftware.com>
2023-07-04 16:04:47 +0530

    Scrolling in Data Grid

---
+++ DevExtremeDataGrid/Js/ScrollAndEdit.js
@@ -0,0 +1,64 @@
+﻿$(() => {
+    let mySotre = new DevExpress.data.CustomStore({
+        key: "id",
+        load: async function () {
+            return await $.ajax({
+                type: "GET",
+                url: "https://jsonplaceholder.typicode.com/todos",
+                success: (e) => {
+                    DevExpress.ui.notify("Data Fetched Successfully", "success", 500);
+                },
+                error: (e) => {
+                    DevExpress.ui.notify("Data Not Fetched", "Error", 500);
+                }
+
+            })
+        }
+    })
+    $("#dxScrollAndEdit").dxDataGrid({
+        dataSource: mySotre,
+        scrolling: {
+            columnRenderingMode: "virtual",
+            mode: "virtual",
+            preLoadEnabled: true,
+            rowRenderingMode: "standard",
+            useNative: false,
+            showScrollbar: "onScroll"
+        },
+        columns: [{
+            dataField: "userId",
+            sortOrdder: 'asc',
+            width: "170px",
+            alignment: "center",
+        },
+        {
+            dataField: "id",
+            showInColumnChooser: false,
+            allowEditing: true,
+            width: "200px",
+            alignment: "center",
+        },
+        {
+            dataField: "title",
+            showInColumnChooser: false,
+            width: "800px",
+            alignment: "center",
+        },
+        {
+            dataField: "completed",
+            width: "180px",
+            alignment: "center",
+        }
+        ],
+        pager: {
+            allowedPageSizes: [5, 8, 15, 30],
+            showInfo: true,
+            showNavigationButtons: true,
+            showPageSizeSelector: true,
+            visible: true,
+        },
+        paging: {
+            pageSize: 20
+        },
+    });
+})
