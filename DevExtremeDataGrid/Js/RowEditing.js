c7f08406d08009fe8176f1c28621389fbe8350ac
Om Joshi <om@rkitsoftware.com>
2023-07-05 17:46:02 +0530

    Row Editing in the DataGrid

---
+++ DevExtremeDataGrid/Js/RowEditing.js
@@ -0,0 +1,88 @@
+﻿$(() => {
+    let mySotre = new DevExpress.data.CustomStore({
+        key: "d01F01",
+        load: async function () {
+            return await $.ajax({
+                type: "GET",
+                url: "https://localhost:7238/api/USED01",
+                success: (e) => {
+                    DevExpress.ui.notify("Data Fetched Successfully", "success", 500);
+                },
+                error: (e) => {
+                    DevExpress.ui.notify("Data Not Fetched", "Error", 500);
+                }
+
+            })
+        }
+    })
+    $("#myDataGrid").dxDataGrid({
+        dataSource: mySotre,
+        showBorders: true,
+         editing: {
+            mode: 'row',
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true,
+        },
+        columns: [{
+            dataField: "d01F01",
+            caption: "User Id",
+            sortOrdder: 'asc',
+            width: "170px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F02",
+            caption: "User First Name",
+            showInColumnChooser: false,
+            allowEditing: true,
+            width: "200px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F03",
+            caption: "User Last Name",
+            showInColumnChooser: false,
+            width: "200px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F04",
+            caption: "User Address",
+            width: "200px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F05",
+            caption: "User phone number",
+            width: "100px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F06",
+            caption: "User Email",
+            showInColumnChooser: false,
+            width: "370px",
+            alignment: "center",
+        },
+        {
+            dataField: "d01F07",
+            caption: "User Password",
+            width: "500px",
+            alignment: "center",
+            showInColumnChooser: false,
+            visible: false
+        },
+        ],
+        pager: {
+            allowedPageSizes: [5, 8, 15, 30],
+            showInfo: true,
+            showNavigationButtons: true,
+            showPageSizeSelector: true,
+            visible: true,
+        },
+        paging: {
+            pageSize: 20
+        },
+    });
+})
