384b4f460c1c8795e28ecb0977481d2c4b0eb708
Om Joshi <om@rkitsoftware.com>
2023-07-06 10:47:12 +0530

    Cascading Lookups

---
+++ DevExtremeDataGrid/Js/CascadingLookup/CascadingLookup.js
@@ -0,0 +1,40 @@
+﻿$(() => {
+    $("#myGrid").dxDataGrid({
+        dataSource: employees,
+        keyExpr: 'ID',
+        showBorders: true,
+        editing: {
+            allowUpdating: true,
+            allowAdding: true,
+            mode: 'row'
+        },
+        onEditorPreparing(e) {
+            if (e.parentType === 'dataRow' && e.dataField === 'CityID') {
+                e.editorOptions.disabled = (typeof e.row.data.StateID !== 'number');
+            }
+        },
+        columns: ["FirstName", "LastName", "position", {
+            dataField: "StateID",
+            caption: "State",
+            lookup: {
+                dataSource: states,
+                valueExpr: "ID",
+                displayExpr: "Name"
+            }
+
+        }, {
+                dataField: "CityID",
+                caption: "City",
+                lookup: {
+                    dataSource(options) {
+                        return {
+                            store: cities,
+                            filter: options.data ? ['StateID', "=", options.data.StateID] : null,
+                        };
+                    },
+                    valueExpr: "ID",
+                    displayExpr: "Name"
+                }
+            },]
+    })
+})
