1d3a079e51e4585140e04d8eb48db70a3cfdc835
Om Joshi <om@rkitsoftware.com>
2023-07-13 17:56:36 +0530

    Overview Of Navigation

--- DevExtremeNevigation/Js/01-SimpleNavigation.js
+++ DevExtremeNevigation/Js/01-SimpleNavigation.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(() => {
+    const dxMenu = $('#menu').dxMenu({
+        dataSource: menuData,
+        hideSubmenuOnMouseLeave: false,
+        displayExpr: 'name',
+        onItemClick(data) {
+            const item = data.itemData;
+            if (item.price) {
+                $('#product-details').removeClass('hidden');
+                $('#product-details > img').attr('src', item.icon);
+                $('#product-details > .price').text(`$${item.price}`);
+                $('#product-details > .name').text(item.name);
+            }
+        },
+    }).dxMenu('instance');  
+})
