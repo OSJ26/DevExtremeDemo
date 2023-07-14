61ddd9ad1857132eab5bdab18bc68c4973dbe622
Om Joshi <om@rkitsoftware.com>
2023-07-14 15:40:56 +0530

    Menu Demo with icon

--- DevExtremeNevigation/Data/01-data.js
+++ DevExtremeNevigation/Data/01-data.js
@@ -4,10 +4,12 @@
     items: [{
         id: '1_1',
         name: 'HD Video Player',
+        icon: '../img/video.png',
         price: 220,
     }, {
         id: '1_2',
         name: 'SuperHD Video Player',
+        icon: '../img/video.png',
         price: 270,
     }],
 }, {
@@ -16,23 +18,32 @@
     items: [{
         id: '2_1',
         name: 'SuperLCD 42',
+        icon: '../img/television.png',
         price: 1200,
     }, {
         id: '2_2',
         name: 'SuperLED 42',
+        icon: '../img/television.png',
+
         price: 1450,
     }, {
         id: '2_3',
         name: 'SuperLED 50',
+        icon: '../img/television.png',
+
         price: 1600,
     }, {
         id: '2_4',
         name: 'SuperLCD 55 (Not available)',
+        icon: '../img/television.png',
+
         price: 1350,
         disabled: true,
     }, {
         id: '2_5',
         name: 'SuperLCD 70',
+        icon: '../img/television.png',
+
         price: 4000,
     }],
 }, {
@@ -41,22 +52,28 @@
     items: [{
         id: '3_1',
         name: '19"',
+        icon: '../img/19.png',
         items: [{
             id: '3_1_1',
             name: 'DesktopLCD 19',
+            icon: '../img/monitor.png',
             price: 160,
         }],
     }, {
         id: '3_2',
         name: '21"',
+        icon: '../img/21.png',
         items: [{
             id: '3_2_1',
             name: 'DesktopLCD 21',
+            icon: '../img/monitor.png',
             price: 170,
         }, {
             id: '3_2_2',
+            icon: '../img/monitor.png',
             name: 'DesktopLED 21',
             price: 175,
+            selectable: false
         }],
     }],
 }, {
@@ -64,10 +81,12 @@
     name: 'Projectors',
     items: [{
         id: '4_1',
+        icon: '../img/projector.png',
         name: 'Projector Plus',
         price: 550,
     }, {
         id: '4_2',
+        icon: '../img/projector.png',
         name: 'Projector PlusHD',
         price: 750,
     }],
--- DevExtremeNevigation/Js/01-SimpleNavigation.js
+++ DevExtremeNevigation/Js/01-SimpleNavigation.js
@@ -3,14 +3,20 @@
     $("#menu1").dxMenu({
         dataSource: menuData,
         displayExpr : 'name',
-        //orientation: 'vertical',
+        orientation: 'horizontal',
         showFirstSubmenuMode: {
             name: 'onHover',
             delay: { show: 0, hide: 500 },
         },
-        //submenuDirection:'rightOrBottom' not working 
-        onItemClick: (e) => {
-            console.log("Items Selected is: " +e.itemData.name);
+        cssClass:'menu' ,
+        onItemClick: (data) => {
+            const item = data.itemData;
+            if (item.price) {
+                $('#product-details').removeClass('hidden');
+                $('#product-details > img').attr('src', item.icon);
+                $('#product-details > .price').text(`$${item.price}`);
+                $('#product-details > .name').text(item.name);
+            }
         },
         onSubmenuHidden: (e) => {
             console.log("Exited From: " +e.rootItem[0].innerText);
