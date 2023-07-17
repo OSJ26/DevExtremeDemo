26ad24aa84bd70b80660b3678a1e69034e237ab2
Om Joshi <om@rkitsoftware.com>
2023-07-17 17:05:15 +0530

    Reconcile Work

---
+++ DevExtremeDataGrid/Js/ReconcileDemo/GridEvent.js
@@ -0,0 +1,123 @@
+﻿$(() => {
+    $('#gridContainer').dxDataGrid({
+        dataSource: employees,
+        keyExpr: 'ID',
+        showBorders: true,
+        rowAlterationEnabled: true,
+        showColumnLine: true, 
+        showRowLine: true,
+        focusedRowEnabled: true,
+        paging: {
+            enabled: false,
+        },
+        editing: {
+            mode: 'row',
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true,
+        },
+        columns: [
+            {
+                dataField: 'Prefix',
+                caption: 'Title',
+            }, 'FirstName',
+            'LastName', {
+                dataField: 'Position',
+                width: 130,
+            }, {
+                dataField: 'StateID',
+                caption: 'State',
+                width: 125,
+                lookup: {
+                    dataSource: states,
+                    displayExpr: 'Name',
+                    valueExpr: 'ID',
+                },
+            }, {
+                dataField: 'BirthDate',
+                dataType: 'date',
+                width: 125,
+            },
+        ],
+        onEditingStart(e) {
+            //console.log(e); //component,element,data,cancel,key
+            console.log('EditingStart');
+        },
+        onInitNewRow(e) {
+            //console.log(e); //component,element,data{emplty}
+            console.log('InitNewRow');
+        },
+        onRowInserting(e) {
+            //console.log(e); //getting data
+            console.log('RowInserting');
+        },
+        onRowInserted(e) {
+            //console.log(e); //getting unique key
+            console.log('RowInserted');
+        },
+        onRowUpdating(e) {
+            //console.log(e); //new,olddata
+            console.log('RowUpdating');
+        },
+        onRowUpdated(e) {
+            //console.log(e); //updated value
+            console.log('RowUpdated');
+        },
+        onRowRemoving(e) {
+            //console.log(e); //removed row data
+            console.log('RowRemoving');
+        },
+        onRowRemoved(e) {
+            //console.log(e); //removed row data
+            console.log('RowRemoved');
+        },
+        onSaving(e) {
+            //console.log(e); //cahnges and promise, cancel
+            console.log('Saving');
+        },
+        onSaved(e) {
+            //console.log(e); //cahnges array
+            console.log('Saved');
+        },
+        onEditCanceling(e) {
+            //console.log(e);
+            console.log('EditCanceling');
+        },
+        onEditCanceled(e) {
+            //console.log(e);
+            console.log('EditCanceled');
+        },
+        onEditorPrepared: (e) => {
+            //console.log(e); //all the information of editor
+            console.log("Editor Prepared");
+        },
+        onEditorPreparing: (e) => {
+            //console.log(e);
+            console.log("Editor Preparing");
+        },
+        onRowPrepared: (e) => {
+            //console.log(e); //All the rows with the data 
+            console.log("Row Prepared");
+        },
+        onFocusedCellChanged: (e) => {
+            //console.log(e);
+            console.log("Cell Focuse Changed")
+        },
+        onFocusedCellChanging: (e) => {
+            //console.log(e);
+            console.log("Cell Focuse Changing")
+        },
+        onFocusedRowChanged: (e) => {
+            console.log("Row Losses Focuse");
+            //console.log("Focused Row Id is: " + e.row.data.ID);
+        },
+        onFocusedRowChanging: (e) => {
+            //console.log(e);
+            console.log("Row Geting the Focuse");
+        },
+        onRowClick: (e) => {
+            //console.log(e);
+            console.log("Row Clicked");
+        },
+    });
+});
---
+++ DevExtremeDataGrid/Js/ReconcileDemo/ReconcileDemo.js
@@ -0,0 +1,69 @@
+﻿$(() => {
+    $('#reconcileWork').dxDataGrid({
+        dataSource: employees,
+        keyExpr: 'ID',
+        allowColumnReordering: true,
+        allowColumnResizing: true,
+        cloumnAutoWidth: true,
+        showBorders: true,
+        focusedRowEnabled: true,
+        rowAlterationEnabled: true,
+        showColumnLine: true,
+        showRowLine: true,
+        columnChooser: {
+            enabled: true,
+            allowSearch: true,
+            mode: 'select',
+            search: {
+                editorOptions: {
+                    placeholder: 'Search column',
+                    mode: 'text'
+                },
+            },
+            selection: {
+                recusive: true,
+                allowSelectAll: true,
+                selectByClick: true
+            }
+        },
+        columnFixing: {
+            enabled: true
+        },
+
+        editing: {
+            mode: 'row',
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true,
+        },
+
+        columns: [{
+            caption: 'Employee',
+            width: 230,
+            allowHiding: false,
+            fixed: true,
+            calculateCellValue(data) {
+                return [data.Title,
+                data.FirstName, data.LastName]
+                    .join(' ');
+            },
+        }, {
+            dataField: 'BirthDate',
+            showEditorAlways: true,
+            dataType: 'date',
+        }, {
+            dataField: 'HireDate',
+            dataType: 'date',
+            showEditorAlways: true,
+        }, {
+            dataField: 'Position',
+            alignment: 'right',
+        }, {
+            dataField: 'Address',
+            width: 230,
+        }, 'City', 'State', {
+            dataField: 'Zipcode',
+            visible: false,
+        }, 'HomePhone', 'MobilePhone', 'Skype', 'Email'],
+    })
+})
