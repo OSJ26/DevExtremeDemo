4a494115cddc30ced92d546a05e290f6fc8e8f61
Om Joshi <om@rkitsoftware.com>
2023-07-19 13:24:21 +0530

    DevExtreme Dialog and Notification Started

---
+++ DevExtremeNotification/Js/PopUp.js
@@ -0,0 +1,123 @@
+﻿$(() => {
+    const popUpTemplate = () => {
+        return $("<div>").append(
+            $(`<p>Full Name: <span>${employee.FirstName}</span>
+                         <span>${employee.LastName}</span></p>`),
+            $(`<p>Birth Date: <span>${employee.BirthDate}</span></p>`),
+            $(`<p>Address: <span>${employee.Address}</span></p>`),
+            $(`<p>Hire Date: <span>${employee.HireDate}</span></p>`),
+            $(`<p>Position: <span>${employee.Position}</span></p>`),
+        );
+    };
+
+    const popup = $('#popup').dxPopup({
+        contentTemplate: popUpTemplate,
+        dragEnabled: false,
+        fullScreen: true,
+        //resizeEnabled: true,
+        showTitle: true,
+        showCloseButton: false,
+        title: 'Information',
+        visible: false,
+        position: {
+            at: 'bottom',
+            my: 'center',
+            collision: 'fit'
+        },
+        animation: {
+            show: {
+                type: 'slide',
+                duration: 400,
+                from: { opacity: 0, top: 100 },
+                to: {
+                    opacity: 1,
+                    position: {
+                        my: "top",
+                        at: "bottom",
+                    }
+                }
+            },
+            hide: {
+                type: 'fade',
+                duration: 500,
+                from: { opacity: 1 },
+                to: {
+                    opacity: 0,
+                }
+            }
+        },
+        toolbarItems: [
+            {
+                locateInMenu: 'always',
+                widget: 'dxButton',
+                toolbar: 'top',
+                options: {
+                    text: 'More Info',
+                    onClick() {
+                        const message = `More info about ${employee.FirstName} ${employee.LastName}`;
+
+                        DevExpress.ui.notify({
+                            message,
+                            position: {
+                                my: 'center top',
+                                at: 'center top',
+                            },
+                        }, 'success', 3000);
+                    }
+                }
+            },
+            {
+                widget: 'dxButton',
+                toolbar: 'bottom',
+                location: 'before',
+                options: {
+                    icon: 'email',
+                    text: 'Send',
+                    onClick() {
+                        const message = `Email is sent to ${employee.FirstName} ${employee.LastName}`;
+                        DevExpress.ui.notify({
+                            message,
+                            position: {
+                                my: 'center top',
+                                at: 'center top',
+                            },
+                        }, 'success', 3000);
+                    },
+                },
+            }, {
+                widget: 'dxButton',
+                toolbar: 'bottom',
+                location: 'after',
+                options: {
+                    text: 'Close',
+                    onClick() {
+                        popup.hide();
+                    },
+                }
+            }
+        ]
+    }).dxPopup('instance');
+
+    employees.forEach((currentEmployee) => {
+        $('<li>').append(
+            $('<img>').attr('src', currentEmployee.Picture).attr('id', `image${currentEmployee.ID}`),
+            $('<br>'),
+            $('<span>').html(`<i>${currentEmployee.FirstName}</i>`),
+            $('<span>').html(` <i>${currentEmployee.LastName}</i>`),
+            $('<br>'),
+            $('<div>')
+                .addClass('button-info')
+                .dxButton({
+                    text: 'Details',
+                    onClick() {
+                        employee = currentEmployee;
+                        popup.option({
+                            contentTemplate: () => popUpTemplate(),
+                            'position.of': `#image${employee.ID}`,
+                        });
+                        popup.show();
+                    },
+                }),
+        ).appendTo($('#employees'));
+    });
+})
---
+++ DevExtremeNotification/Js/Toast.js
@@ -0,0 +1,32 @@
+﻿$(() => {
+    const toast = $('#toast').dxToast({
+        animation: {
+            show: {
+                type: 'fade',
+                duration: 400,
+                from: 0,
+                to: 1
+            },
+            hide: {
+                type: 'fade',
+                duration: 400,
+                from: 1,
+                to: 0
+            }
+        },
+        closeOnClick: true,
+        displayTime: 5000,
+        height: 50,
+        message: 'This is Demo Of Toast',
+        position: 'bottom',
+        width: 300,
+    }).dxToast('instance');
+
+    $('#btn').dxButton({
+        text: 'ClickMe',
+        onClick: () => {
+            toast.show();
+        }
+    })
+
+})
